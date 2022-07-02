e284bea3d13601dd71c91070b0ca6df78988c62f
Spahr Michael <spahr.michael@protonmail.com>
2022-07-02 17:51:39 +0200

    add some comments for Trie construction

--- merkle-patricia-trie/exercise.py
+++ merkle-patricia-trie/exercise.py
@@ -5,16 +5,23 @@ from ethereum import trie, utils, db
 ## EX1
 print("Exercice 1 ... \n")
 
-# initialize trie
+# initialize empty trie
 state = trie.Trie(db.EphemDB())
-key = utils.to_string("\x01\x01\x02")
 
+# add one entry
+key = utils.to_string("\x01\x01\x02")
 state.update(key, rlp.encode(["hello"]))
 print("Root hash: ", encode(state.root_hash, "hex"))
 
 k, v = state.root_node
 print("Root node: ", [k, v])
 print("HP encoded key, in hex:", encode(k, "hex"))
+print(
+    "- Note the final 6 nibbles are the key we used, 010102, while the first two give us the HP encoding."
+)
+# The first nibble tells us that this is a terminator node (since it would be 10 in binary,
+# so the second least significant bit is on),
+# and since the key was even length (least significant bit is 0), we add a second 0 nibble.
 
 
 primaryRoot = encode(state.root_hash, "hex")
@@ -24,12 +31,14 @@ input("\n\nExercice 2 - A... \n")
 
 # initialize trie from previous hash; add new entry with same key.
 state = trie.Trie(state.db, decode(primaryRoot, "hex"))
+print("- we initialize a trie that starts with the previous hash")
 print("Root hash: ", encode(state.root_hash, "hex"))
 print("Root node: ", state.root_node)
 
 print("\n")
 
 state.update(key, rlp.encode(["hellothere"]))
+print("- we add an entry with the same key we already used but a different value")
 print("Root hash: ", encode(state.root_hash, "hex"))
 print("Root node: ", state.root_node)
 # we now have two tries, addressed in the database by their respective hashes, though they each have the same key
@@ -39,21 +48,31 @@ print("Root node: ", state.root_node)
 input("\n\nExercice 2 - B... \n")
 
 state = trie.Trie(state.db, decode(primaryRoot, "hex"))
+print(
+    "- Now, let’s add an entry that use’s the same key but with a different final nibble"
+)
 key = utils.to_string("\x01\x01\x03")
 
 state.update(key, rlp.encode(["hellothere"]))
 print("Root hash:", encode(state.root_hash, "hex"))
 k, v = state.root_node
 
-print("Root node: " + str([k, v]) + "\n")
-
+print("Root node: " + str([k, v]))
+print(
+    f"- Ext. node? {state._get_node_type(state.root_node) == trie.NODE_TYPE_EXTENSION}\n"
+)
 
 print("HP encoded key, in hex:", encode(k, "hex"))
-print(state._get_node_type(state.root_node) == trie.NODE_TYPE_EXTENSION)
+
 
 common_prefix_key, node_hash = state.root_node
 print("Node hash : ", state._decode_to_node(node_hash))
-print(state._get_node_type(state._decode_to_node(node_hash)) == trie.NODE_TYPE_BRANCH)
+print(
+    "- this is a branch node with 17 entries, entires 2 and 3 contain non empty node (our two keys 010102 and 010103)"
+)
+print(
+    f"- Branch node? {state._get_node_type(state._decode_to_node(node_hash)) == trie.NODE_TYPE_BRANCH}"
+)
 
 
 ## EX2 - C
@@ -65,12 +84,15 @@ print("Root hash: ", encode(state.root_hash, "hex"))
 print("Root node: ", state.root_node)
 
 print("\n")
+print(
+    "- Let’s do it again but with a key equal to the first few nibbles of our original ke"
+)
 key = utils.to_string("\x01\x01")
 state.update(key, rlp.encode(["hellothere"]))
 
 print("Root hash:", encode(state.root_hash, "hex"))
 print("Root node:", state.root_node)
-print("Branch node: ", state._decode_to_node(state.root_node[1]))
+print("new Branch node: ", state._decode_to_node(state.root_node[1]))
 
 
 ## EX2 - D
