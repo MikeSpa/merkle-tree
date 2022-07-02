29fcf8a663aa9b7d29a3b35c2c4f861e65b92a91
Spahr Michael <spahr.michael@protonmail.com>
2022-07-02 03:03:39 +0200

    initial commit

---
+++ merkle_proof.py
@@ -0,0 +1,26 @@
+from utils import *
+import math
+from node import Node
+
+
+def merkle_proof(tx, merkle_tree):
+    """Given a tx and a Merkle tree object, retrieve its list of tx's and
+    parse through it to arrive at the minimum amount of information required
+    to arrive at the correct block header. This does not include the tx
+    itself.
+
+    Return this data as a list; remember that order matters!
+    """
+    #### YOUR CODE HERE
+
+
+
+def verify_proof(tx, merkle_proof):
+    """Given a Merkle proof - constructed via `merkle_proof(...)` - verify
+    that the correct block header can be retrieved by properly hashing the tx
+    along with every other piece of data in the proof in the correct order
+    """
+    #### YOUR CODE HERE
+
+
+    
